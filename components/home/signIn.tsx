b8e84225fbbb9e6e045b63e5ec258f2a55911492
C S Vaishakh <vaishakhsubramonian@gmail.com>
2025-08-01 12:07:40 +0530

    feat : added signin adn signup forms(tested working)

---
+++ components/home/signIn.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function SignIn () {
+    const [password, setPassword] = useState("")
+    const [email, setEmail] = useState("")
+    const [resMsg, setResMsg] = useState("")
+    const [pop, setPop] = useState(false)
+
+    const handleSubmit = async (e:React.FormEvent) => {
+        e.preventDefault()
+        try {
+            const res = await axios.post('http://127.0.0.1:5000/signin',{ email,password })
+            console.log(res.data)
+            setResMsg("SignIn Successful")
+            setPop(true)
+        }catch (error : unknown){
+            if(axios.isAxiosError(error)){
+                setResMsg(error.message)
+                setPop(true)
+            }
+        }
+        setTimeout(()=>{
+            setPop(false)
+        },3000)
+    }
+
+    return (
+        <section>
+            {pop && (
+                <div>
+                    {resMsg}
+                </div>
+            )}
+
+            <form className="auth-form" onSubmit={handleSubmit}>
+                <input 
+                    className="form-input" 
+                    type="email" 
+                    placeholder="Email" 
+                    value={email} 
+                    onChange={e => setEmail(e.target.value)}
+                    required
+                />
+                <input 
+                    className="form-input" 
+                    type="password" 
+                    placeholder="Password" 
+                    value={password} 
+                    onChange={e => setPassword(e.target.value)}
+                    required
+                />
+                <button className="submit-button" type="submit">SignIn</button>
+            </form>
+        </section>
+    )
+}
