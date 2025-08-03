3145bd77a4bc8a76670fc987628c7690e45e5e58
C S Vaishakh <vaishakhsubramonian@gmail.com>
2025-08-03 17:18:25 +0530

    feat: added profile page with data fetching and display

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { useAuthStore } from "@/store/auth";
 import ProtectedRoute from "@/components/protectedRoute";
 import React from "react";
 import SignOut from "@/components/signOut";
+import Link from "next/link";
 
 export default function Dashboard() {
   const token = useAuthStore((state) => state.token);
@@ -16,6 +17,7 @@ export default function Dashboard() {
         <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
         <p>Welcome! User ID: {user}</p>
         <SignOut/>
+        <button><Link href={'/dashboard/profile'}>Profile</Link></button>
       </div>
     </ProtectedRoute>
   );
