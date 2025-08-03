7529b7318cfc5477c7579dba5b4d57fe887be617
C S Vaishakh <vaishakhsubramonian@gmail.com>
2025-08-03 17:15:21 +0530

    refactor: move AuthState type definition to types file

--- types/types.ts
+++ types/types.ts
@@ -4,3 +4,21 @@ export interface Credentials {
     Role_code : string
     Name : string
 }
+
+export type AuthState = {
+    token : string | null
+    user : string | null
+    setToken : (token : string) => void
+    setUser : (user : string) => void
+    clearToken : () => void
+    logout : () => void
+}
+
+
+export interface Profile {
+    userid : string 
+	email : string 
+	name : string 
+	role : string 
+	created_at : string 
+}
